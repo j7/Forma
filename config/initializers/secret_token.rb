78f0cae9d9d5de7dab43a15996ea439ea595d8ab
Javier Carballo <javier@javier.im>
2014-02-17 12:18:21 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forma::Application.config.secret_key_base = '0325eab3c888754ee5a20cb600cd0db89e2cfd4809684c882221deb8ea0a9af73fb2177819f315b839a901e5ae70693f72ddbc7fb87bbd963c2e43d4e928b40f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Forma::Application.config.session_store :cookie_store, key: '_Forma_session'
